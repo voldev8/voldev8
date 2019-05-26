23d87a13e727896cdc756013374ec1917e2fc960
voldev8 <47670926+voldev8@users.noreply.github.com>
2019-05-25 20:24:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var whySukha_btn = document.getElementById('whySukha_btn');
-var whySukha = document.getElementById('whySukha');
+var whySukha = document.getElementsByClassName('whySukha')[0];
 var whySukha_close = document.getElementById('whySukha_close');
-
+var main = document.getElementsByClassName('main')[0];
 whySukha_btn.addEventListener('click', ()=>{
   whySukha.style.visibility = "visible";
+  main.style.visibility = "collapse";
 });
 whySukha_close.addEventListener('click', ()=>{
   whySukha.style.visibility = "collapse";
+  main.style.visibility = "visible";
 });
